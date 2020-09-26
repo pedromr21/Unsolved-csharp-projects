4e48d6e47606786e920f196957379712d589d182
Pedro <pedr0374@edu.easj.dk>
2020-09-27 00:10:14 +0200

    finished the exercise

--- Unsolved/DiceGame/DiceGame/InsertCodeHere.cs
+++ Unsolved/DiceGame/DiceGame/InsertCodeHere.cs
@@ -1,10 +1,20 @@
-﻿namespace DiceGame
+﻿using System;
+
+namespace DiceGame
 {
     public class InsertCodeHere
     {
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+            DiceCup cup = new DiceCup();
+
+            Console.WriteLine($"The sum of the dice is {cup.TotalValue}");
+
+            cup.Shake();
+
+            Console.WriteLine($"The sum of the dice is {cup.TotalValue}");
+
 
             // The LAST line of code should be ABOVE this line
         }
