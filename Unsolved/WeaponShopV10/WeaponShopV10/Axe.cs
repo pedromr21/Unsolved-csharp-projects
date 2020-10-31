99af65a16a06d9544bdf79b0b21328568c60e874
Pedro <pedr0374@edu.easj.dk>
2020-10-31 22:15:34 +0100

    Solved WeaponShopV10

--- Unsolved/WeaponShopV10/WeaponShopV10/Axe.cs
+++ Unsolved/WeaponShopV10/WeaponShopV10/Axe.cs
@@ -1,4 +1,6 @@
-﻿namespace WeaponShopV10
+﻿using System;
+
+namespace WeaponShopV10
 {
     /// <summary>
     /// This class represents a Axe. An Axe is 
@@ -13,7 +15,30 @@
         public Axe(string description) 
             : base(description, InitialAxeMinDamage, InitialAxeMaxDamage)
         {
-        } 
+        }
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+
+        public int DamageFromAxe()
+        {
+            int damage = CalculateDamage();
+
+            MinDamage = MinDamage < 3 ? 0 : MinDamage - 3;
+            MaxDamage = MaxDamage < 3 ? 0 : MaxDamage - 3;
+
+            return damage;
+        }
+
+        public void Sharpen()
+        {
+            MinDamage = InitialAxeMinDamage;
+            MaxDamage = InitialAxeMaxDamage;
+        }
+
         #endregion
     }
 }
--- Unsolved/WeaponShopV10/WeaponShopV10/Wand.cs
+++ Unsolved/WeaponShopV10/WeaponShopV10/Wand.cs
@@ -1,4 +1,6 @@
-﻿namespace WeaponShopV10
+﻿using System;
+
+namespace WeaponShopV10
 {
     /// <summary>
     /// This class represents a Wand. A Wand is 
@@ -6,6 +8,7 @@
     /// </summary>
     public class Wand : Weapon
     {
+        private bool _isEnchanted = false;
         public const int InitialWandMinDamage = 10;
         public const int InitialWandMaxDamage = 30;
 
@@ -13,7 +16,29 @@
         public Wand(string description) 
             : base(description, InitialWandMinDamage, InitialWandMaxDamage)
         {
-        } 
+        }
+        #endregion
+
+        #region Properties
+        public bool IsEnchanted
+        {
+            get { return _isEnchanted; }
+            set { _isEnchanted = value; }
+        }
+        #endregion
+
+        #region Methods
+         public int DamageFromWand()
+         {
+             if (IsEnchanted)
+             {
+                 return CalculateDamage() * 2;
+             }
+             else
+             {
+                 return CalculateDamage();
+             }
+         }
         #endregion
     }
 }
