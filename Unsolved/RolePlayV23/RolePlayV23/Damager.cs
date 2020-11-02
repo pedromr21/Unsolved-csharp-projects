4dd3db1d2db1d1b5997dc177faee1363964d8902
Pedro <pedr0374@edu.easj.dk>
2020-11-02 21:57:15 +0100

    Solved, added damager and created the characters in insertcodehere

---
+++ Unsolved/RolePlayV23/RolePlayV23/Damager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlayV23
+{
+    public class Damager : Character
+    {
+        public Damager(string name, int hitPoints, int minDamage, int maxDamage)
+            : base(name, hitPoints, minDamage, maxDamage)
+        {
+                
+        }
+
+        protected override int DealDamageModifyChance
+        {
+            get { return 40; }
+        }
+
+        protected override int CalculateModifiedDealDamage(int dealtDamage)
+        {
+            return dealtDamage * 2;
+        }
+    }
+}
