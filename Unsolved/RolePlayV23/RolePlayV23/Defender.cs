d9a92bd27388e2261b85fb5410b968e5dec83b31
Pedro <pedr0374@edu.easj.dk>
2020-11-02 21:31:35 +0100

    Added defender class, overriding the chance to receive damage and reduce it by 50%

---
+++ Unsolved/RolePlayV23/RolePlayV23/Defender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlayV23
+{
+    public class Defender : Character
+    {
+        public Defender(string name, int hitPoints, int minDamage, int maxDamage)
+            :base(name, hitPoints, minDamage, maxDamage)
+        {
+
+        }
+
+        protected override int ReceiveDamageModifyChance
+        {
+            get { return 45; }
+        }
+
+        protected override int CalculateModifiedReceivedDamage(int receivedDamage)
+        {
+            return receivedDamage / 2;
+        }
+    }
+}
